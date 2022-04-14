f9f7e0ab462741137b601153986e008e3b23f7fe
opethdeliverance <stephen.jwalsh@yahoo.com>
2022-04-14 21:27:18 -0230

    Plugin working well, added options for user.

--- JQuery Plugin/js.js
+++ JQuery Plugin/js.js
@@ -3,8 +3,15 @@
 $(document).ready( () => {
 
 	console.log("js file");
-	$("#image_list").imageRandPlus();
+	$("#image_list").imageRandPlus({
+		 imageNum: 4,
+		 imageWidth: "400px",
+		 imageHeight: "400px",
+
+		 // to add a new image to ImageRandPLUS, 
+		 //follow the instructions on the HTML file. 
+		 
+	});
 
 	
-
 }); 
